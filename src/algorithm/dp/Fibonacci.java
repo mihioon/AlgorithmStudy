ed2cb16dca10d677d07e9af77e48e25e1e60a19c
mihioon <rangee3014@gmail.com>
2024-07-16 23:00:55 +0900

    [코딩테스트연습Day6]피보나치 2 - 코드수정 1차

--- src/algorithm/dp/Fibonacci.java
+++ src/algorithm/dp/Fibonacci.java
@@ -6,29 +6,33 @@ import java.util.Scanner;
 
 public class Fibonacci {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
         //n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작성하시오.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
 
-        List<Integer> integerList = new ArrayList<>();
-        integerList.add(0);
-        integerList.add(1);
         if(n == 0){
-            System.out.println(integerList.get(0));
+            System.out.println(0);
             return;
         }
         if(n == 1){
-            System.out.println(integerList.get(1));
+            System.out.println(1);
             return;
         }
+        int first = 0;
+        int second = 1;
         int order = 2;
         int nthNum = 0;
         while ( order <= n ) {
-            nthNum = integerList.get(order-2)+integerList.get(order-1);
-            integerList.add(nthNum);
+            nthNum = first+second;
+            first=second;
+            second=nthNum;
             order++;
         }
         System.out.println(nthNum);
+        long end = System.currentTimeMillis();
+        System.out.println("수행시간: " + (end - start) + " ms");
     }
 }
